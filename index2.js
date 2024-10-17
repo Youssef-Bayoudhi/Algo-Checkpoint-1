72f7b0a34afece9d349c309bd35732e2982affdf
Youssef-Bayoudhi <youssefjow033@gmail.com>
2024-10-17 16:16:45 +0100

    checkpoint algo//js done

---
+++ index2.js
@@ -0,0 +1,14 @@
+function insertsort (array){
+
+    for(var i=1 ;i<array.length;i++){
+    var x =array[i]
+    var j = i-1
+    while(j>=0 && array[j] >x ){
+        array[j+1]=array[j]
+        j--
+    }
+    array[j+1]= x
+    }
+return array
+
+}
